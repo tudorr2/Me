47cb053c1b295f108bbfb209f942a09c03490fe2
Neagu Tudor <tudorneagu7@gmail.com>
2025-03-04 20:46:29 +0200

    removed contact form

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-
-
 //sidemenu
 var sidmenu = document.getElementById("topmenu");
 
-function openmenu(){
-    sidmenu.style.right ="0";
+function openmenu() {
+  sidmenu.style.right = "0";
 }
-function closemenu(){
-    sidmenu.style.right ="-200px";
+function closemenu() {
+  sidmenu.style.right = "-200px";
 }
-
 
 
